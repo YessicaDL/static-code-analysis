5cc06770099d482a4167d0ebeed6ddb13b028086
YessicaDL <22151191@aguascalientes.tecnm.mx>
2026-05-20 08:48:30 -0600

    fix: update assertion in addition test to check for value greater than 0

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -27,6 +27,6 @@ class ExampleTest extends TestCase
 
         $this->assertEquals(13, $result);
 
-        $this->assertGreaterThan(4, $result);
+        $this->assertGreaterThan(0, $result);
     }
 }
